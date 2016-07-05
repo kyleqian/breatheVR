1b2566846555a03c9fdcb7004cc314028ba67f5f
Kyle Qian <cqian@stanford.edu>
2016-07-05 00:41:05 -0700

    Very hacky GameCircle fade

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,54 +5,117 @@ using LitJson;
 
 public class GameController : MonoBehaviour {
     public GameObject GameAnchor;
+    private Color GameCircleColor;
     private const float ScaleMin = 0.0f;
     private const float ScaleMax = 10.0f;
-    private bool breatheIn = true; // Start with inhale
 
     private double currentTime = 0;
     private double timeOfNextBeat;
     private double timeOfMeasureStart;
     private int measureStartIndex = 4; // Starting beat
     private const int BeatsPerMeasure = 4;
-    private int nextBeatIndex; 
+    private int nextBeatIndex;
     private JsonData beatTimes;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         string jsonString = File.ReadAllText(Application.dataPath + "/Data/-8587340713348999394_beats_0.json");
         beatTimes = JsonMapper.ToObject(jsonString)["beatTimes"];
         nextBeatIndex = measureStartIndex;
         timeOfMeasureStart = (double)beatTimes[nextBeatIndex];
         timeOfNextBeat = timeOfMeasureStart;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        GameCircleColor = GameAnchor.GetComponentInChildren<Renderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update() {
         currentTime = Time.time;
-        GameAnchor.transform.localScale = GetTransformScale();
+        TransformGameCircle();
 
         if (currentTime >= timeOfNextBeat) {
             //print(currentTime.ToString());
-            if (nextBeatIndex == measureStartIndex + BeatsPerMeasure) {
+            if (nextBeatIndex == measureStartIndex + 2*BeatsPerMeasure) {
                 measureStartIndex = nextBeatIndex;
                 timeOfMeasureStart = (double)beatTimes[measureStartIndex];
-                breatheIn = !breatheIn;
             }
             nextBeatIndex++;
             timeOfNextBeat = (double)beatTimes[nextBeatIndex];
         }
     }
 
-    Vector3 GetTransformScale() {
+    void TransformGameCircle() {
         double lengthOfMeasure = (double)beatTimes[measureStartIndex + BeatsPerMeasure] - timeOfMeasureStart;
         double secondsIntoMeasure = currentTime - timeOfMeasureStart;
         float scale = 0;
+        float alpha = 1;
         if (secondsIntoMeasure > 0) {
             double percentIntoMeasure = secondsIntoMeasure / lengthOfMeasure;
-            double scalePercentage = breatheIn ? percentIntoMeasure : 1.0f - percentIntoMeasure;
-            scale = (float)((ScaleMax - ScaleMin) * (scalePercentage) + ScaleMin);
+            scale = (float)((ScaleMax - ScaleMin) * (percentIntoMeasure) + ScaleMin);
+
+            if (percentIntoMeasure > 1) {
+                float hack = 1.0f - Mathf.Min(100, Mathf.Pow((float)(percentIntoMeasure - 1.0f) * 100, 2))/100.0f;
+                alpha = hack;
+            }
         }
-        return new Vector3(scale, scale, 0);
+        GameAnchor.transform.localScale = new Vector3(scale, scale, 0);
+        GameAnchor.GetComponentInChildren<Renderer>().material.color = new Color(GameCircleColor.r, GameCircleColor.g, GameCircleColor.b, alpha);
     }
 }
+
+
+//public class GameController : MonoBehaviour {
+//    public GameObject GameAnchor;
+//    private const float ScaleMin = 0.0f;
+//    private const float ScaleMax = 10.0f;
+//    private bool breatheIn = true; // Start with inhale
+
+//    private double currentTime = 0;
+//    private double timeOfNextBeat;
+//    private double timeOfMeasureStart;
+//    private int measureStartIndex = 4; // Starting beat
+//    private const int BeatsPerMeasure = 4;
+//    private int nextBeatIndex; 
+//    private JsonData beatTimes;
+
+
+//	// Use this for initialization
+//	void Start () {
+//        string jsonString = File.ReadAllText(Application.dataPath + "/Data/-8587340713348999394_beats_0.json");
+//        beatTimes = JsonMapper.ToObject(jsonString)["beatTimes"];
+//        nextBeatIndex = measureStartIndex;
+//        timeOfMeasureStart = (double)beatTimes[nextBeatIndex];
+//        timeOfNextBeat = timeOfMeasureStart;
+//	}
+
+//	// Update is called once per frame
+//	void Update () {
+//        currentTime = Time.time;
+//        GameAnchor.transform.localScale = GetTransformScale();
+
+//        if (currentTime >= timeOfNextBeat) {
+//            //print(currentTime.ToString());
+//            if (nextBeatIndex == measureStartIndex + BeatsPerMeasure) {
+//                measureStartIndex = nextBeatIndex;
+//                timeOfMeasureStart = (double)beatTimes[measureStartIndex];
+//                breatheIn = !breatheIn;
+//            }
+//            nextBeatIndex++;
+//            timeOfNextBeat = (double)beatTimes[nextBeatIndex];
+//        }
+//    }
+
+//    // GameCircle goes up and down
+//    // TODO: LERP THIS?
+//    Vector3 GetTransformScale() {
+//        double lengthOfMeasure = (double)beatTimes[measureStartIndex + BeatsPerMeasure] - timeOfMeasureStart;
+//        double secondsIntoMeasure = currentTime - timeOfMeasureStart;
+//        float scale = 0;
+//        if (secondsIntoMeasure > 0) {
+//            double percentIntoMeasure = secondsIntoMeasure / lengthOfMeasure;
+//            double scalePercentage = breatheIn ? percentIntoMeasure : 1.0f - percentIntoMeasure;
+//            scale = (float)((ScaleMax - ScaleMin) * (scalePercentage) + ScaleMin);
+//        }
+//        return new Vector3(scale, scale, 0);
+//    }
+//}
