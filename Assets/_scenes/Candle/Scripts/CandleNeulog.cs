14d0555cb7d1617c0025894d5975e53df88f7900
Kyle Qian <cqian@stanford.edu>
2016-07-07 23:05:37 -0700

    Added candle scene

---
+++ Assets/_scenes/Candle/Scripts/CandleNeulog.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using SimpleJSON;
+
+[RequireComponent (typeof(Light))]
+[RequireComponent (typeof(ParticleAnimator))]
+public class CandleNeulog : MonoBehaviour {
+
+    public Light lightSource;
+    public ParticleAnimator candleFlame;
+
+    const int MinLightSpotAngle = 30;
+    const int MaxLightSpotAngle = 80;
+
+    const float MinLightIntensity = 0.20f;
+    const float MaxLightIntensity = 5.0f;
+
+    const float MinFlameSizeGrow = -1.0f;
+    const float MaxFlameSizeGrow = -0.10f;
+
+    private HttpWebRequest request;
+	private const string Url = "http://localhost:22002/NeuLogAPI?GetSensorValue:[Respiration],[1]";
+    private int nullCounter = 0;
+    private const int APICallsPerSecond = 40;
+
+    private string message = "";
+
+	private int airPressure = 0;
+	private int lowPressure = int.MaxValue;
+	private int highPressure = int.MinValue;
+	private int pressureRange = int.MinValue;
+
+    private float preCalibrationSeconds = 2.0f;
+	private float calibrationSeconds = 10.0f;
+	private bool calibrating = true;
+
+	void Start() {
+        // The API seems to be capped somewhere around 30-40 calls/s
+        InvokeRepeating("QueryAPI", 0.0f, 1.0f/APICallsPerSecond);
+        message = "Calibrating...";
+
+        // Defaults for candle
+        lightSource.spotAngle = MinLightSpotAngle;
+        lightSource.intensity = MinLightIntensity;
+        candleFlame.sizeGrow = MinFlameSizeGrow;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (calibrating) {
+            // Precalibration
+            if (preCalibrationSeconds > 0) {
+                preCalibrationSeconds -= Time.smoothDeltaTime;
+                return;
+            }
+
+            // Records the range of pressure
+            calibrationSeconds -= Time.smoothDeltaTime;
+			if (calibrationSeconds > 0) {
+				//message = highPressure.ToString() + " to " + lowPressure.ToString();
+
+				if (airPressure < lowPressure && airPressure > 0) lowPressure = airPressure;
+				if (airPressure > highPressure) highPressure = airPressure;
+			}
+            if (calibrationSeconds <= 0) {
+				pressureRange = highPressure - lowPressure;
+				calibrating = false;
+			}
+		}
+
+        if (!calibrating) {
+            float doubleBoundedPercentageOfMax = Mathf.Max(0.0f, Mathf.Min(1.0f, (float)(airPressure - lowPressure) / pressureRange));
+            //float lowerBoundedPercentageOfMax = Mathf.Max(0.0f, (float)(airPressure - lowPressure) / pressureRange);
+            message = doubleBoundedPercentageOfMax.ToString("F2");
+
+            // Candle behavior
+            lightSource.spotAngle = (MaxLightSpotAngle - MinLightSpotAngle) * doubleBoundedPercentageOfMax + MinLightSpotAngle;
+            lightSource.intensity = (MaxLightIntensity - MinLightIntensity) * doubleBoundedPercentageOfMax + MinLightIntensity;
+            candleFlame.sizeGrow = (MaxFlameSizeGrow - MinFlameSizeGrow) * doubleBoundedPercentageOfMax + MinFlameSizeGrow;
+        }
+    }
+
+    // Coroutine to ping the Neulog API
+    void QueryAPI() {
+		WWW www = new WWW (Url);
+        StartCoroutine(WaitForAPIResponse(www));
+	}
+
+    // Handles the API responses
+    IEnumerator WaitForAPIResponse(WWW www) {
+        yield return www;
+
+        var responseJSON = JSON.Parse(www.text);
+        if (responseJSON != null) {
+            airPressure = responseJSON["GetSensorValue"][0].AsInt;
+        } else {
+            // Probably means over API limit
+            nullCounter++;
+            print(nullCounter.ToString());
+        }
+    }
+
+	void OnGUI() {
+		int w = Screen.width, h = Screen.height;
+
+		GUIStyle style = new GUIStyle();
+
+		Rect rect = new Rect(0, 0, w, h * 2 / 100);
+		style.alignment = TextAnchor.UpperLeft;
+		style.fontSize = h * 6 / 100;
+		style.normal.textColor = new Color(0, 0, 0, 1.0f);
+		string text = message;
+		GUI.Label(rect, text, style);
+	}
+}
